15debc3eecce6fc0a6057b8b9b86497f73828c4f
Jason <328053730@qq.com>
2015-06-05 11:04:47 +0800

    backend

---
+++ backend/config.php
@@ -0,0 +1,20 @@
+<?php
+define('DIR_APPLICATION', APP_PATH.'backend/');
+define('DIR_LANGUAGE', DIR_APPLICATION.'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION.'view/template/');
+
+define('DIR_ASSET', APP_PATH.'asset/');
+define('DIR_IMAGE', DIR_ASSET.'image/');
+define('DIR_DOWNLOAD', DIR_ASSET.'download/');
+define('DIR_UPLOAD', DIR_ASSET.'upload/');
+
+define('TPL_JS', '/asset/javascript/');
+define('TPL_IMG', '/asset/image/');
+
+define('DIR_SYSTEM', APP_PATH.'system/');
+define('DIR_CACHE', DIR_SYSTEM.'cache/');
+define('DIR_CONFIG', DIR_SYSTEM.'config/');
+
+define('DIR_LOGS', DIR_SYSTEM.'logs/');
+
+define('DIR_FRONT', APP_PATH.'member/');
---
+++ backend/language/chinese/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer'] = '消防e站 <a href="http://www.yk119.cn">苏州源控</a> &copy; 2011-' . date('Y') . ' All Rights Reserved.<br />Version %s';
+?>
