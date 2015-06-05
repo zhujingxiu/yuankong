d44d4340b581edaf1056f3a3e8cd22e439e6cfaa
Jason <328053730@qq.com>
2015-06-05 11:15:54 +0800

    base code

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+define('DIR_APPLICATION', APP_PATH.'market/');
+define('DIR_LANGUAGE', DIR_APPLICATION.'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION.'view/theme/');
+
+define('DIR_ASSET', APP_PATH.'asset/');
+define('DIR_IMAGE', DIR_ASSET.'image/');
+define('DIR_DOWNLOAD', DIR_ASSET.'download/');
+define('DIR_UPLOAD', DIR_ASSET.'upload/');
+
+define('TPL_JS', 'asset/javascript/');
+define('TPL_IMG', 'asset/image/');
+
+define('DIR_SYSTEM', APP_PATH.'system/');
+define('DIR_CACHE', DIR_SYSTEM.'cache/');
+define('DIR_CONFIG', DIR_SYSTEM.'config/');
+
+define('DIR_LOGS', DIR_SYSTEM.'logs/');
+
